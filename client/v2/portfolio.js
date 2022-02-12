851d7828dd99b48da9f91106b3fff056bcbb71fc
GitBrunoCode <brunopincet@hotmail.fr>
2022-02-12 17:25:28 +0100

    feat(FEATURE):Sort asc desc price date

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -1,32 +1,31 @@
 // Invoking strict mode https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
-'use strict';
+"use strict";
 
 // current products on the page
 let currentProducts = [];
 let currentPagination = {};
-let allProducts=[];
-let current_brand="all";
-
+let allProducts = [];
+let current_brand = "all";
+let current_sort = "normal";
 
 // instantiate the selectors
-const selectShow = document.querySelector('#show-select');
-const selectPage = document.querySelector('#page-select');
-const sectionProducts = document.querySelector('#products');
-const spanNbProducts = document.querySelector('#nbProducts');
-const selectBrand = document.querySelector('#brand-select');
-const checkprice= document.querySelector('#reasonable_price');
-const checkreleased= document.querySelector('#recently_released');
-
+const selectShow = document.querySelector("#show-select");
+const selectPage = document.querySelector("#page-select");
+const sectionProducts = document.querySelector("#products");
+const spanNbProducts = document.querySelector("#nbProducts");
+const selectBrand = document.querySelector("#brand-select");
+const checkprice = document.querySelector("#reasonable_price");
+const checkreleased = document.querySelector("#recently_released");
+const selectSort = document.querySelector("#sort-select");
 
 /**
  * Set global value
  * @param {Array} result - products to display
  * @param {Object} meta - pagination meta info
  */
-const setCurrentProducts = ({result, meta}) => {
+const setCurrentProducts = ({ result, meta }) => {
   currentProducts = result;
   currentPagination = meta;
-  
 };
 
 /**
@@ -44,13 +43,13 @@ const fetchProducts = async (page = 1, size = 12) => {
 
     if (body.success !== true) {
       console.error(body);
-      return {currentProducts, currentPagination};
+      return { currentProducts, currentPagination };
     }
 
     return body.data;
   } catch (error) {
     console.error(error);
-    return {currentProducts, currentPagination};
+    return { currentProducts, currentPagination };
   }
 };
 
@@ -58,17 +57,16 @@ const fetchProducts = async (page = 1, size = 12) => {
  * Render list of products
  * @param  {Array} products
  */
-const renderProducts = products => {
-  if(selectBrand.value!="all"){
-    products=GetProdbyBrand(selectBrand.value)
+const renderProducts = (products) => {
+  if (selectBrand.value != "all") {
+    products = GetProdbyBrand(selectBrand.value);
   }
   const fragment = document.createDocumentFragment();
   // if products to display
-  if(products.length!=0)
-  {
-    const div = document.createElement('div');
+  if (products.length != 0) {
+    const div = document.createElement("div");
     const template = products
-      .map(product => {
+      .map((product) => {
         return `
         <div class="product" id=${product.uuid}>
           <span>${product.brand}</span>
@@ -77,34 +75,32 @@ const renderProducts = products => {
         </div>
       `;
       })
-      .join('');
-  
+      .join("");
+
     div.innerHTML = template;
     fragment.appendChild(div);
-    sectionProducts.innerHTML = '<h2>Products</h2>';
+    sectionProducts.innerHTML = "<h2>Products</h2>";
     sectionProducts.appendChild(fragment);
-  }
-  else // to correct display bug
-  {
-    const div = document.createElement('div');
+  } // to correct display bug
+  else {
+    const div = document.createElement("div");
     div.innerHTML = "no products available";
     fragment.appendChild(div);
-    sectionProducts.innerHTML = '<h2>Products</h2>';
+    sectionProducts.innerHTML = "<h2>Products</h2>";
     sectionProducts.appendChild(fragment);
   }
-  
 };
 
 /**
  * Render page selector
  * @param  {Object} pagination
  */
-const renderPagination = pagination => {
-  const {currentPage, pageCount} = pagination;
+const renderPagination = (pagination) => {
+  const { currentPage, pageCount } = pagination;
   const options = Array.from(
-    {'length': pageCount},
+    { length: pageCount },
     (value, index) => `<option value="${index + 1}">${index + 1}</option>`
-  ).join('');
+  ).join("");
 
   selectPage.innerHTML = options;
   selectPage.selectedIndex = currentPage - 1;
@@ -114,8 +110,8 @@ const renderPagination = pagination => {
  * Render page selector
  * @param  {Object} pagination
  */
-const renderIndicators = pagination => {
-  const {count} = pagination;
+const renderIndicators = (pagination) => {
+  const { count } = pagination;
 
   spanNbProducts.innerHTML = count;
 };
@@ -126,7 +122,6 @@ const render = (products, pagination) => {
   renderIndicators(pagination);
 };
 
-
 /**
  * Declaration of all Listeners
  */
@@ -134,153 +129,224 @@ const render = (products, pagination) => {
 /**
  * Select the number of products to display
  */
-selectShow.addEventListener('change', async (event) => {  
-  currentProducts=allProducts.result
-  currentProducts=GetCorrectProd(currentPagination.currentPage,parseInt(event.target.value),current_brand)
-  currentPagination.pageCount=parseInt(event.target.value)
+selectShow.addEventListener("change", async (event) => {
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    parseInt(event.target.value),
+    current_brand
+  );
+  currentPagination.pageCount = parseInt(event.target.value);
   render(currentProducts, currentPagination);
 });
 
 /**
  * Select the page to display
  */
-selectPage.addEventListener('change', async (event) => {
-  currentPagination.currentPage=parseInt(event.target.value)
-  currentProducts=allProducts.result
-  currentProducts=GetCorrectProd(currentPagination.currentPage,currentPagination.pageCount,current_brand)
+selectPage.addEventListener("change", async (event) => {
+  currentPagination.currentPage = parseInt(event.target.value);
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    currentPagination.pageCount,
+    current_brand
+  );
   render(currentProducts, currentPagination);
 });
 
 /**
  * [FILTER] Select the brand to display
  */
- selectBrand.addEventListener('change', async (event) => {
-    currentProducts=allProducts.result
-    currentProducts=GetCorrectProd(currentPagination.currentPage,currentPagination.pageCount,event.target.value)
-    current_brand=event.target.value
-    renderProducts(currentProducts,currentPagination)
+selectBrand.addEventListener("change", async (event) => {
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    currentPagination.pageCount,
+    event.target.value
+  );
+  current_brand = event.target.value;
+  renderProducts(currentProducts, currentPagination);
 });
 
 /**
- * [Checkbox] Products with price < 50 
+ * [Checkbox] Products with price < 50
  */
- checkprice.addEventListener('change', async (event) => {
-  currentProducts=allProducts.result
-  currentProducts=GetCorrectProd(currentPagination.currentPage,currentPagination.pageCount,current_brand)
-  renderProducts(currentProducts,currentPagination)
-});
-
-checkreleased.addEventListener('change', async (event) => {
-  currentProducts=allProducts.result
-  currentProducts=GetCorrectProd(currentPagination.currentPage,currentPagination.pageCount,current_brand)
-  renderProducts(currentProducts,currentPagination)
+checkprice.addEventListener("change", async (event) => {
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    currentPagination.pageCount,
+    current_brand
+  );
+  renderProducts(currentProducts, currentPagination);
 });
 
 /**
- * [Checkbox] Products with released date < 2 weeks
+ * [Checkbox] released less than 2 weeks ago
  */
+checkreleased.addEventListener("change", async (event) => {
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    currentPagination.pageCount,
+    current_brand
+  );
+  renderProducts(currentProducts, currentPagination);
+});
 
-document.addEventListener('DOMContentLoaded', async () => {
+/**
+ * [Checkbox] released less than 2 weeks ago
+ */
+selectSort.addEventListener("change", async (event) => {
+  current_sort = event.target.value;
+  currentProducts = allProducts.result;
+  currentProducts = GetCorrectProd(
+    currentPagination.currentPage,
+    currentPagination.pageCount,
+    current_brand
+  );
+  renderProducts(currentProducts, currentPagination);
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
   const products = await fetchProducts();
   const a = await fetchProducts(1, 999);
-  allProducts=a;
+  allProducts = a;
   setProductsOptionFilter(allProducts);
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
 
-
-
 /**
  * Declaration of various function to answer features
  */
 
-function setProductsOptionFilter(products){
-  let brand_name=GetBrands(products["result"])
-  for (let i=0;i<brand_name.length;i++){
-    var x=document.getElementById("brand-select");
+function setProductsOptionFilter(products) {
+  let brand_name = GetBrands(products["result"]);
+  for (let i = 0; i < brand_name.length; i++) {
+    var x = document.getElementById("brand-select");
     // create option using DOM
-    const newOption = document.createElement('option');
+    const newOption = document.createElement("option");
     const optionText = document.createTextNode(brand_name[i]);
     // set option text
     newOption.appendChild(optionText);
     // and option value
-    newOption.setAttribute('value',brand_name[i]);
+    newOption.setAttribute("value", brand_name[i]);
     // add the option to the select box
     x.appendChild(newOption);
   }
 }
 
-function GetBrands(marketplace){
-  let brand_name=[];
-  for (let i=0; i<marketplace.length;i++)
-  {
-    if (!brand_name.includes(marketplace[i].brand))
-    {
+function GetBrands(marketplace) {
+  let brand_name = [];
+  for (let i = 0; i < marketplace.length; i++) {
+    if (!brand_name.includes(marketplace[i].brand)) {
       brand_name.push(marketplace[i].brand);
     }
   }
-  return(brand_name);
+  return brand_name;
 }
 
-function GetCorrectProd(nb_page,len_page,brand)
-{
-  let dico=GetProdbyBrand(brand)
-  let new_prod=[]
-  for(let i=(nb_page-1)*(len_page-1);i<(nb_page-1)*(len_page-1)+(len_page)-1;i++)
-  {
-    if (dico[i]==null){
+function GetCorrectProd(nb_page, len_page, brand) {
+  let dico = GetProdbyBrand(brand);
+  let new_prod = [];
+  for (
+    let i = (nb_page - 1) * (len_page - 1);
+    i < (nb_page - 1) * (len_page - 1) + len_page - 1;
+    i++
+  ) {
+    if (dico[i] == null) {
       break;
-    }
-    else{
-      new_prod.push(dico[i])
-    }
-  }
-  console.log("new",new_prod)
-  return new_prod
-}
-
-function GetProdbyBrand(brand){
-  let product_list=[];
-  let products=[]
-  if (checkreleased.checked==true && checkprice.checked==true){
-    products=FilterDatePrice(currentProducts.slice())
-  }
-  else if (checkprice.checked==true){
-    products=FilterPrice(currentProducts.slice())
-
-  }
-  else if (checkreleased.checked==true){
-    products=FilterDate(currentProducts.slice())
-  }
-  else
-  {
-    products=currentProducts
-  }
-  for (let i=0;i<products.length;i++)
-  {
-    if(brand=='all'){
-      product_list.push(products[i])
-    }
-    else if (products[i].brand == brand){
-      product_list.push(products[i])
+    } else {
+      new_prod.push(dico[i]);
     }
   }
-  return product_list
+
+  return new_prod;
 }
 
-function FilterPrice(product){
-  let products=product.filter(x=>x.price<50)
-  return products
+function GetProdbyBrand(brand) {
+  let product_list = [];
+  let products = [];
+
+  if (checkreleased.checked == true && checkprice.checked == true) {
+    products = FilterDatePrice(currentProducts.slice());
+  } else if (checkprice.checked == true) {
+    products = FilterPrice(currentProducts.slice());
+  } else if (checkreleased.checked == true) {
+    products = FilterDate(currentProducts.slice());
+  } else {
+    products = currentProducts;
+  }
+
+  if (current_sort == "price-asc") {
+    SortByPrice(products, true);
+  } else if (current_sort == "price-desc") {
+    SortByPrice(products, false);
+  } else if (current_sort == "date-asc") {
+    SortByDate(products, true);
+  } else if (current_sort == "date-desc") {
+    SortByDate(products, false);
+  }
+
+  for (let i = 0; i < products.length; i++) {
+    if (brand == "all") {
+      product_list.push(products[i]);
+    } else if (products[i].brand == brand) {
+      product_list.push(products[i]);
+    }
+  }
+  return product_list;
 }
 
-function FilterDate(product){
-  let products=product.filter(x=>new Date(x.released).getTime()>new Date(Date.now() - 12096e5))
-  return products
+function FilterPrice(product) {
+  let products = product.filter((x) => x.price < 50);
+  return products;
 }
 
-function FilterDatePrice(product)
-{
-  let products=product.filter(x=>new Date(x.released).getTime()>new Date(Date.now() - 12096e5) && x.price<50)
-  return products
+function FilterDate(product) {
+  let products = product.filter(
+    (x) => new Date(x.released).getTime() > new Date(Date.now() - 12096e5)
+  );
+  return products;
+}
+
+function FilterDatePrice(product) {
+  let products = product.filter(
+    (x) =>
+      new Date(x.released).getTime() > new Date(Date.now() - 12096e5) &&
+      x.price < 50
+  );
+  return products;
+}
+
+function SortByPrice(product, up) {
+  if (up == true) {
+    Object.assign({}, product.sort(price_sort_up));
+  } else {
+    Object.assign({}, product.sort(price_sort_down));
+  }
+}
+
+function SortByDate(product, up) {
+  if (up == true) {
+    Object.assign({}, product.sort(date_sort_up));
+  } else {
+    Object.assign({}, product.sort(date_sort_down));
+  }
+}
+
+function price_sort_up(a, b) {
+  return a.price - b.price;
+}
+function price_sort_down(a, b) {
+  return b.price - a.price;
+}
+
+function date_sort_up(a, b) {
+  return new Date(a.released).getTime() - new Date(b.released).getTime();
+}
+
+function date_sort_down(a, b) {
+  return new Date(b.released).getTime() - new Date(a.released).getTime();
 }
