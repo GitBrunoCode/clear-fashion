5a968471c7f076879d08d421346e9b6d86beaf71
GitBrunoCode <brunopincet@hotmail.fr>
2022-03-04 18:52:46 +0100

    feat(shop): scrap save file

--- server/sandbox.js
+++ server/sandbox.js
@@ -2,25 +2,41 @@
 const dedicatedbrand = require('./sources/dedicatedbrand');
 const adresseparis = require('./sources/adresseparis');
 const montlimart = require('./sources/montlimart');
+const fs = require('fs');
+const products_list = require('./products.json');
 
-// async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
-async function sandbox (eshop = 'https://adresse.paris/630-toute-la-collection') {
-// async function sandbox (eshop = 'https://www.montlimart.com/toute-la-collection.html?limit=all') {
+let = brand_dico={ "dedicatedbrand":"https://www.dedicatedbrand.com/en/men/news",
+"adresseparis":"https://adresse.paris/630-toute-la-collection",
+"montlimart":"https://www.montlimart.com/toute-la-collection.html?limit=all"}
 
 
+async function sandbox (eshop) {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
-  //  const products = await dedicatedbrand.scrape(eshop);
-   const products = await adresseparis.scrape(eshop);
-  //  const products=await montlimart.scrape(eshop);
+    console.log(`🕵️‍♀️  browsing all sources`);
+    let products = await dedicatedbrand.scrape(brand_dico["dedicatedbrand"]);
+    products = products.concat(await adresseparis.scrape(brand_dico["adresseparis"]));
+    products = products.concat(await montlimart.scrape(brand_dico["montlimart"]));
     console.log(products);
-    console.log('done');
+    console.log("all brands have been scraped")
+    for(let j=0;j<products.length;j++)
+    {
+      if(contains(products_list,"uuid",products[j]["uuid"])==false)
+      {
+        products_list.push(products[j]);
+      }
+    }
+    fs.writeFileSync("./products.json", JSON.stringify(products_list));
+    
     process.exit(0);
+
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 }
+
+
+
 
 const [,, eshop] = process.argv;
 
@@ -28,3 +44,9 @@ sandbox(eshop);
 
 
 
+function contains(arr, key, val) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][key] === val) return true;
+  }
+  return false;
+}
