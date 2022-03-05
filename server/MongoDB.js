21ef4685e72fc6ac9a2987dc0423fc7c532c0fc0
GitBrunoCode <brunopincet@hotmail.fr>
2022-03-05 15:17:10 +0100

    feat(MongoDB): insert products & queries

---
+++ server/MongoDB.js
@@ -0,0 +1,88 @@
+const { MongoClient } = require("mongodb");
+const products = require("./sources/products.json");
+
+async function Connection() {
+  const MONGODB_URI =
+    "mongodb+srv://user:clear-fashion@cluster0.2j3o0.mongodb.net/myFirstDatabase?retryWrites=true&writeConcern=majority";
+  const MONGODB_DB_NAME = "Cluster0";
+
+  try {
+    const client = await MongoClient.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+    });
+    const db = client.db(MONGODB_DB_NAME);
+    console.log("connected");
+    return db;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function InsertProducts() {
+  const db = await Connection();
+  const collection = db.collection("products");
+  const result = collection.insertMany(products);
+  console.log(result);
+}
+
+async function Prod_by_brand(brand) {
+  const db = await Connection();
+  const collection = db.collection("products");
+  collection
+    .find({
+      brand: brand,
+    })
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
+
+async function Prod_lt_price(price) {
+  const db = await Connection();
+  const collection = db.collection("products");
+  collection.find({ price: { $lt: price } }).toArray(function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+
+async function Prod_by_price() {
+  const db = await Connection();
+  const collection = db.collection("products");
+  collection
+    .aggregate([{ $sort: { price: 1 } }])
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
+
+async function Prod_by_date() {
+  const db = await Connection();
+  const collection = db.collection("products");
+  collection
+    .aggregate([{ $sort: { date: -1 } }])
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
+
+async function Prod_lt_2w() {
+  const db = await Connection();
+  const collection = db.collection("products");
+  collection
+    .find({ date: { $gt: new Date(Date.now() - 12096e5).toISOString() } })
+    .toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
+
+// InsertProducts();
+// Prod_by_brand("loom");
+// Prod_lt_price(50);
+// Prod_by_price();
+// Prod_by_date();
+// Prod_lt_2w();
